0b206180b7c979e06023902eae10943fa20465c2
omrilotan <omrilotan@hotmail.com>
2025-10-22 01:15:38 +0100

    Move things around the Contribute page

--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -33,43 +33,12 @@ export default () => (
 			</figure>
 		</article>
 		<article id="the-pasture-is-a-community-space">
-			<h3>Make a charitable donation</h3>
 			<p>
-				We receive no funding from the local council and rely on donations to
-				undertake our work. By donating, you're not just supporting a green
-				space—you're helping us continue to protect biodiversity, maintain safe
-				access, and sustain a valuable community resource. Every contribution,
-				no matter how small, will make a real difference in maintaining this
-				precious pasture for future generations.
-			</p>
-			<p className="center">
-				<DonateButton />
-			</p>
-
-			<p>
-				Long Lane Pasture Trust is a charity registered in England & Wales (№
-				1122236) and is run in accordance with its Trust Deed. Charity details
-				are available on{" "}
-				<a
-					href="https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/4035882"
-					target="_blank"
-					rel="noopener"
-					aria-label="Long Lane Pasture Trust on the registry"
-				>
-					the registry
-				</a>
-				.{" "}
-			</p>
-			<p>
-				Cheques can be sent by mail to No. 256 Long Lane, London N3 2RN. If you
-				would like to make a donation any other way, please <ContactUsLink />.
+				The pasture is a community space maintained by community members to the
+				benefit of all. There are many ways you can take part in caring for the
+				pasture.
 			</p>
 			<Volunteer>
-				<p>
-					The pasture is a community space maintained by community members to
-					the benefit of all. There are many ways you can take part in caring
-					for the pasture.
-				</p>
 				<p>
 					Help with the maintenance of the pasture. Trim, Weed, Cut, Rake, Mow,
 					Plant, Prune, Clean, Maintain…
@@ -91,6 +60,38 @@ export default () => (
 			<div className="contributors">
 				<DynamicVolunteers />
 			</div>
+			<HorizontalLine />
+			<h3>Make a charitable donation</h3>
+			<p>
+				We receive no funding from the local council and rely on donations to
+				undertake our work. By donating, you're not just supporting a green
+				space—you're helping us continue to protect biodiversity, maintain safe
+				access, and sustain a valuable community resource. Every contribution,
+				no matter how small, will make a real difference in maintaining this
+				precious pasture for future generations.
+			</p>
+			<p className="center">
+				<DonateButton />
+			</p>
+			<p>
+				Long Lane Pasture Trust is a charity registered in England & Wales (№
+				1122236) and is run in accordance with its Trust Deed. Charity details
+				are available on{" "}
+				<a
+					href="https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/4035882"
+					target="_blank"
+					rel="noopener"
+					aria-label="Long Lane Pasture Trust on the registry"
+				>
+					the registry
+				</a>
+				.{" "}
+			</p>
+			<p>
+				Cheques can be sent by mail to No. 256 Long Lane, London N3 2RN. If you
+				would like to make a donation any other way, please <ContactUsLink />.
+			</p>
+			<HorizontalLine />
 			<h3>Initiate educational or community projects</h3>
 			<p>
 				We welcome proposals for educational or community projects that can
