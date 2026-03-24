45d768aa7a59040d7da1bd378fa0e042a0c9c0dd
omrilotan <omrilotan@users.noreply.github.com>
2026-03-24 13:49:53 +0000

    😎 Autofix

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -2,6 +2,6 @@ declare type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
 
 // CSS module declarations
 declare module "*.css" {
-  const content: Record<string, string>;
-  export default content;
+	const content: Record<string, string>;
+	export default content;
 }
