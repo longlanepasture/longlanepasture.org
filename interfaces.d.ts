c2627613992be982d24536e24db63d733a33f36d
omrilotan <omri@hamadgera.com>
2026-03-24 13:49:15 +0000

    Fix next version

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -1,1 +1,7 @@
 declare type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
+
+// CSS module declarations
+declare module "*.css" {
+  const content: Record<string, string>;
+  export default content;
+}
